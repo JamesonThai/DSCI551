02ccbe111c68b55d5614c6c2b6ed5556f5f1ac59
Emma <tripwithsoo@gmail.com>
2020-11-20 04:42:21 -0800

    - show bar chart with value - add explanation of CrimeScore

--- DSCI551_Project_LA/main.py
+++ DSCI551_Project_LA/main.py
@@ -67,14 +67,14 @@ def line():
     if json_obj["housing"] and len(json_obj["housing"]) != 0:
         pd_housing = search_neighborhoods_features(json_obj["housing"], ["HousingScore"]).to_dict()
     if json_obj["act"] and len(json_obj["act"]) != 0:
-        pd_act = search_neighborhoods_features(json_obj["act"], ["AvgACT", "AvgScrEng", "AvgScrMath"]).to_dict()
+        pd_act = search_neighborhoods_features(json_obj["act"], ["AvgACT", "AvgScrEng", "AvgScrMath", "AvgScrRead","AvgScrSci"]).to_dict()
 
     print(pd_crime)
     # out = {"crime": pd_crime, "housing": pd_housing, "act": pd_act}
     fig1 = createLayout(pd_act, "ACT Score", "Region", "ACT score per region")
     fig2 = createLayout(pd_housing, "Housing Score", "Region", "Housing score per region")
-    fig3 = createLayout({"CrimeCount": pd_crime['CrimeCount']}, "Crime Count", "Region", "Crime count per region")
-    fig4 = createLayout({"CrimeScore": pd_crime['CrimeScore']}, "Crime Score", "Region", "Crime score per region", [0,1])
+    fig3 = createLayout({"CrimeCount": (pd_crime['CrimeCount'] if 'CrimeCount' in pd_crime else {})}, "Crime Count", "Region", "Crime count per region")
+    fig4 = createLayout({"CrimeScore": (pd_crime['CrimeScore'] if 'CrimeScore' in pd_crime else {})}, "Crime Score", "Region", "Crime score per region", [0,0.3])
 
     fig_json = json.dumps([fig1,fig2,fig3,fig4], cls=py.utils.PlotlyJSONEncoder)
     return render_template('chart_bar.html', graphJSON=fig_json)
@@ -102,6 +102,7 @@ def search_neighborhoods_features(NEIGHBORHOODS, FEATURES):
 def createLayout(data, title, xaxis, yaxis, range = None):
     traces, idx = [], 0
 
+    list_neigh, list_values = [], []
     for k, v in data.items():
         list_neigh, list_values = [], []
         for k1, v1 in v.items():
@@ -109,16 +110,24 @@ def createLayout(data, title, xaxis, yaxis, range = None):
             if v1 is None: v1 = 0
             list_values.append(v1)
 
-        traces.append(go.Bar(name=k, x=list_neigh, y=list_values, offsetgroup=idx))
+        traces.append(go.Bar(name=k, x=list_neigh, y=list_values, offsetgroup=idx, text=list_values, textposition='outside'))
         idx += 1
+
+    res_min, res_max = 0, 1
+    if list_values:
+        res_min = min(float(sub) for sub in list_values)
+        res_max = max(float(sub) for sub in list_values)
 
     if range is not None:
         layout = go.Layout(title=title, xaxis=dict(title=xaxis), yaxis=dict(title=yaxis, range=range))
     else:
-        layout = go.Layout(title=title, xaxis=dict(title=xaxis), yaxis=dict(title=yaxis))
+        layout = go.Layout(title=title, xaxis=dict(title=xaxis), yaxis=dict(title=yaxis, range=[res_min, res_max + res_max/6]))
     data = traces
-    fig = go.Figure(data=data, layout=layout)
 
+    # total_labels = [{"x": x, "y": total} for x, total in zip(list_values, list_neigh)]
+
+    fig = go.Figure(data=data, layout=layout)
+    # fig = fig.update_layout(annotations=total_labels)
     return fig
 
 if __name__ == "__main__":
